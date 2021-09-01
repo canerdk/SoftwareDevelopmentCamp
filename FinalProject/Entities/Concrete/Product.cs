750cf41fc2909fcf9072e5513a845432c154830c
caner <canerdk1995@hotmail.com>
2021-09-01 21:09:53 +0300

    added entities

---
+++ FinalProject/Entities/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
