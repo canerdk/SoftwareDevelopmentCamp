fd16009f00aab57c0edc72806a5a8e1f2c7bcb47
caner <canerdk1995@hotmail.com>
2021-09-09 23:59:22 +0300

    configured cors policy

--- FinalProject/WebAPI/Startup.cs
+++ FinalProject/WebAPI/Startup.cs
@@ -42,7 +42,7 @@ namespace WebAPI
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowOrigin",
-                    builder => builder.WithOrigins("http://localhost:4000"));
+                    builder => builder.WithOrigins("http://localhost:4200"));
             });
 
 
@@ -76,7 +76,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost:4000").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
